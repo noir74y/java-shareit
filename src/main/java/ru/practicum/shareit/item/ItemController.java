dcfb0d7576761acccb9e856f58a4a92a2ba2118e
noir74y <noir74@yandex.ru>
2023-07-02 22:31:55 +0300

    feat: add logging

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.model.ItemDtoReq;
@@ -10,9 +11,7 @@ import ru.practicum.shareit.validation.OnCreate;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 
-/**
- * TODO Sprint add-controllers.
- */
+@Slf4j
 @RestController
 @RequestMapping("/items")
 @RequiredArgsConstructor
@@ -22,26 +21,31 @@ public class ItemController {
 
     @PostMapping
     public ItemDtoResp create(@Validated(OnCreate.class) @RequestBody ItemDtoReq itemDtoReq, @RequestHeader("X-Sharer-User-Id") @NotNull int ownerId) {
+        log.info("POST /items/ {}", itemDtoReq);
         return itemMapper.item2dtoResp(itemService.create(itemMapper.dtoReq2item(itemDtoReq), ownerId));
     }
 
     @PatchMapping("/{itemId}")
     public ItemDtoResp update(@RequestBody ItemDtoReq itemDtoReq, @RequestHeader("X-Sharer-User-Id") @NotNull int ownerId, @PathVariable int itemId) {
+        log.info("PATCH /items/" + itemId + " {}", itemDtoReq);
         return itemMapper.item2dtoResp(itemService.update(itemMapper.dtoReq2item(itemDtoReq), ownerId, itemId));
     }
 
     @GetMapping("/{itemId}")
     public ItemDtoResp findById(@PathVariable int itemId) {
+        log.info("GET /items/" + itemId);
         return itemMapper.item2dtoResp(itemService.findById(itemId));
     }
 
     @GetMapping
     public ArrayList<ItemDtoResp> findByOwner(@RequestHeader("X-Sharer-User-Id") int ownerId) {
+        log.info("GET /items/" + ownerId);
         return itemMapper.bulkItem2dtoResp(itemService.findByOwner(ownerId));
     }
 
     @GetMapping("/search")
     public ArrayList<ItemDtoResp> findByText(@RequestParam(value = "text") String text) {
+        log.info("GET /search/" + text);
         return itemMapper.bulkItem2dtoResp(itemService.findByText(text));
     }
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.user;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.model.UserDtoReq;
@@ -10,6 +11,7 @@ import ru.practicum.shareit.validation.OnUpdate;
 
 import java.util.ArrayList;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "/users")
 @RequiredArgsConstructor
@@ -19,26 +21,31 @@ public class UserController {
 
     @PostMapping
     public UserDtoResp create(@Validated(OnCreate.class) @RequestBody UserDtoReq userDtoReq) {
+        log.info("POST /users/ {}", userDtoReq);
         return userMapper.user2dtoResp(userService.create(userMapper.dtoReq2user(userDtoReq)));
     }
 
     @PatchMapping("/{userId}")
     public UserDtoResp update(@Validated(OnUpdate.class) @RequestBody UserDtoReq userDtoReq, @PathVariable int userId) {
+        log.info("PATCH /users/" + userId + " {}", userDtoReq);
         return userMapper.user2dtoResp(userService.update(userMapper.dtoReq2user(userDtoReq), userId));
     }
 
     @DeleteMapping("/{userId}")
     public void delete(@PathVariable int userId) {
+        log.info("DELETE /users/" + userId);
         userService.delete(userId);
     }
 
     @GetMapping("/{userId}")
     public UserDtoResp find(@PathVariable int userId) {
+        log.info("GET /users/" + userId);
         return userMapper.user2dtoResp(userService.findById(userId));
     }
 
     @GetMapping
     public ArrayList<UserDtoResp> findAll() {
+        log.info("GET /users/");
         return userMapper.bulkUser2dtoResp(userService.findAll());
     }
 }
