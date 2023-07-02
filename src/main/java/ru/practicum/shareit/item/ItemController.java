5cf5c5a2b5130b6feeba9f160f37fb0cf1886fba
noir74y <noir74@yandex.ru>
2023-07-02 23:43:47 +0300

    ref: add ItemConstant

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -20,13 +20,13 @@ public class ItemController {
     private final ItemMapper itemMapper;
 
     @PostMapping
-    public ItemDtoResp create(@Validated(OnCreate.class) @RequestBody ItemDtoReq itemDtoReq, @RequestHeader("X-Sharer-User-Id") @NotNull int ownerId) {
+    public ItemDtoResp create(@Validated(OnCreate.class) @RequestBody ItemDtoReq itemDtoReq, @RequestHeader(ItemConstant.HEADER_USER_ID) @NotNull int ownerId) {
         log.info("POST /items/ {}", itemDtoReq);
         return itemMapper.item2dtoResp(itemService.create(itemMapper.dtoReq2item(itemDtoReq), ownerId));
     }
 
     @PatchMapping("/{itemId}")
-    public ItemDtoResp update(@RequestBody ItemDtoReq itemDtoReq, @RequestHeader("X-Sharer-User-Id") @NotNull int ownerId, @PathVariable int itemId) {
+    public ItemDtoResp update(@RequestBody ItemDtoReq itemDtoReq, @RequestHeader(ItemConstant.HEADER_USER_ID) @NotNull int ownerId, @PathVariable int itemId) {
         log.info("PATCH /items/" + itemId + " {}", itemDtoReq);
         return itemMapper.item2dtoResp(itemService.update(itemMapper.dtoReq2item(itemDtoReq), ownerId, itemId));
     }
@@ -38,7 +38,7 @@ public class ItemController {
     }
 
     @GetMapping
-    public ArrayList<ItemDtoResp> findByOwner(@RequestHeader("X-Sharer-User-Id") int ownerId) {
+    public ArrayList<ItemDtoResp> findByOwner(@RequestHeader(ItemConstant.HEADER_USER_ID) int ownerId) {
         log.info("GET /items/" + ownerId);
         return itemMapper.bulkItem2dtoResp(itemService.findByOwner(ownerId));
     }
