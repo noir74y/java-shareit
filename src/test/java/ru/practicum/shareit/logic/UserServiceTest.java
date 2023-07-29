7fc378111cb29bcd680e44ed72b043d97cbb6f2a
noir74y <noir74@yandex.ru>
2023-07-29 23:56:46 +0300

    ref: UserServiceTest without checking of mapper

--- src/test/java/ru/practicum/shareit/logic/UserServiceTest.java
+++ src/test/java/ru/practicum/shareit/logic/UserServiceTest.java
@@ -3,10 +3,10 @@ package ru.practicum.shareit.logic;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import ru.practicum.shareit.mock.MapperMock;
 import ru.practicum.shareit.mock.RepositoryMock;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.model.UserEntity;
@@ -22,62 +22,55 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringJUnitConfig(UserServiceImpl.class)
-@Import({MapperMock.class, RepositoryMock.class})
+@Import({UserMapper.class, ModelMapper.class, RepositoryMock.class})
 public class UserServiceTest {
     int userId = 1;
-    @Autowired
-    protected MapperMock mapperMock;
     @Autowired
     protected RepositoryMock repositoryMock;
     @Autowired
     private UserServiceImpl service;
-    private UserMapper mapper;
     private UserRepository repository;
-    private User referenceModel;
-    private UserEntity referenceEntity;
+    private User user;
+    private UserEntity userEntity;
 
     @BeforeEach
     void setUp() {
-        mapper = mapperMock.getUserMapper();
         repository = repositoryMock.getUserRepository();
-        referenceModel = User.builder().id(userId).name("user").email("user@user.com").build();
-        referenceEntity = UserEntity.builder().id(userId).name("user").email("user@user.com").build();
+        user = User.builder().id(userId).name("user").email("user@user.com").build();
+        userEntity = UserEntity.builder().id(userId).name("user").email("user@user.com").build();
     }
 
     @Test
     void create() throws Throwable {
-        when(mapper.model2entity(any())).thenReturn(referenceEntity);
-        when(mapper.entity2model(any())).thenReturn(referenceModel);
-        when(repository.save(any())).thenReturn(referenceEntity);
+        when(repository.save(any())).thenReturn(userEntity);
 
         assertThat(
-                service.create(referenceModel),
-                equalTo(referenceModel)
+                service.create(user),
+                equalTo(user)
         );
 
-        Mockito.verify(mapper, Mockito.times(1)).model2entity(referenceModel);
-        Mockito.verify(mapper, Mockito.times(1)).entity2model(referenceEntity);
-        Mockito.verify(repository, Mockito.times(1)).save(referenceEntity);
-
-        Mockito.verifyNoMoreInteractions(mapper, repository);
+        Mockito.verify(repository, Mockito.times(1)).save(userEntity);
+        Mockito.verifyNoMoreInteractions(repository);
     }
 
     @Test
     void update() throws Throwable {
-        when(mapper.model2entity(any())).thenReturn(referenceEntity);
-        when(mapper.entity2model(any())).thenReturn(referenceModel);
-        when(repository.save(any())).thenReturn(referenceEntity);
-        when(repository.findById(any())).thenReturn(Optional.ofNullable(referenceEntity));
+        var changedUser = User.builder().id(userId).name("user2").email("user2@user.com").build();
+        var changedUserEntity = UserEntity.builder().id(userId).name("user2").email("user2@user.com").build();
+
+        when(repository.findById(any())).thenReturn(Optional.ofNullable(userEntity));
+        when(repository.save(any())).thenReturn(changedUserEntity);
+
+        var returnValue = service.update(changedUser, userId);
 
         assertThat(
-                service.update(referenceModel, userId),
-                equalTo(referenceModel)
+                returnValue,
+                equalTo(changedUser)
         );
 
-        Mockito.verify(mapper, Mockito.times(1)).model2entity(referenceModel);
-        Mockito.verify(mapper, Mockito.times(2)).entity2model(referenceEntity);
-        Mockito.verify(repository, Mockito.times(1)).save(referenceEntity);
+        Mockito.verify(repository, Mockito.times(1)).save(changedUserEntity);
         Mockito.verify(repository, Mockito.times(1)).findById(userId);
+        Mockito.verifyNoMoreInteractions(repository);
     }
 
 
