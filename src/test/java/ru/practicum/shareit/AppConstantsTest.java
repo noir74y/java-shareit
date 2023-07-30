6ee91c9e2c2c93ab128bfffeb23cf83f278f75e7
noir74y <noir74@yandex.ru>
2023-07-30 22:16:39 +0300

    feat: AppConstantsTest is added

---
+++ src/test/java/ru/practicum/shareit/AppConstantsTest.java
@@ -0,0 +1,17 @@
+package ru.practicum.shareit;
+
+import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.utils.AppConstants;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class AppConstantsTest {
+    @Test
+    void checkAppConstants() {
+        assertThat("X-Sharer-User-Id", equalTo(AppConstants.HEADER_USER_ID));
+        assertThat("yyyy-MM-dd'T'hh:mm:ss", equalTo(AppConstants.DATE_TIME_FORMAT));
+        assertThat("0", equalTo(AppConstants.OFFSET_DEFAULT));
+        assertThat("999", equalTo(AppConstants.PAGE_SIZE_MAX));
+    }
+}
