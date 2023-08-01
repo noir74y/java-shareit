dfe5e72c84faf3caef5d4057d87401a7d72bed66
noir74y <noir74@yandex.ru>
2023-08-02 01:42:26 +0300

    feat: BookingJsonTest is done

---
+++ src/test/java/ru/practicum/shareit/json/BookingDtoTest.java
@@ -0,0 +1,78 @@
+package ru.practicum.shareit.json;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+import ru.practicum.shareit.booking.model.BookingDtoReq;
+import ru.practicum.shareit.booking.model.BookingDtoResp;
+import ru.practicum.shareit.item.model.ItemDtoReq;
+import ru.practicum.shareit.item.model.ItemDtoResp;
+import ru.practicum.shareit.utils.validation.OnCreate;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@JsonTest
+public class BookingDtoTest {
+    BookingDtoReq dtoReqObject;
+    JsonContent<BookingDtoReq> dtoReqJsonContent;
+    @Autowired
+    private JacksonTester<BookingDtoReq> dtoReqTester;
+
+    @BeforeEach
+    void setUp() {
+        dtoReqObject =  BookingDtoReq.builder()
+                .itemId(1)
+                .startDate(LocalDateTime.now().plusDays(1))
+                .endDate(LocalDateTime.now().plusDays(2)).build();
+    }
+
+    @Test
+    void testDtoReq_ItemIdIsNull() throws IOException {
+        dtoReqObject.setItemId(null);
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<BookingDtoReq>> violations = validator.validate(dtoReqObject);
+
+        assertEquals(new ArrayList<>(violations).get(0).getMessage(),
+                "не должно равняться null");
+    }
+
+    @Test
+    void testDtoReq_StartDateIsInThePast() throws IOException {
+        dtoReqObject.setStartDate(LocalDateTime.now().minusDays(1));
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<BookingDtoReq>> violations = validator.validate(dtoReqObject);
+
+        assertEquals(new ArrayList<>(violations).get(0).getMessage(),
+                "должно содержать сегодняшнее число или дату, которая еще не наступила");
+    }
+
+    @Test
+    void testDtoReq_EndDateIsInThePast() throws IOException {
+        dtoReqObject.setEndDate(LocalDateTime.now().minusDays(1));
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<BookingDtoReq>> violations = validator.validate(dtoReqObject);
+
+        assertEquals(new ArrayList<>(violations).get(0).getMessage(),
+                "должно содержать дату, которая еще не наступила");
+    }
+}
