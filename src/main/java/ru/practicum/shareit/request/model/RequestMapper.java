6b8ff790e3c4b6a2f5a6a3e172d2a1f64479a41f
noir74y <noir74@yandex.ru>
2023-08-03 17:38:25 +0300

    ref: some business logic is moved from RequestMapper to RequestServiceImpl

--- src/main/java/ru/practicum/shareit/request/model/RequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/model/RequestMapper.java
@@ -18,7 +18,6 @@ import java.util.stream.Collectors;
 public class RequestMapper {
     private final ModelMapper modelMapper;
     private final ItemRepository itemRepository;
-    private final ItemMapper itemMapper;
 
     public RequestEntity dtoReq2entity(RequestDtoReq dtoReq, Integer requestorId) {
         var entity = modelMapper.map(dtoReq, RequestEntity.class);
@@ -31,41 +30,5 @@ public class RequestMapper {
 
     public RequestDtoResp entity2dtoResp(RequestEntity entity) {
         return modelMapper.map(entity, RequestDtoResp.class);
-    }
-
-    public List<RequestDtoResp> bulkEntity2dtoResp(List<RequestEntity> requestEntities) {
-        Set<Integer> requestIdSet = requestEntities.stream().map(RequestEntity::getId).collect(Collectors.toSet());
-
-        Map<Integer, List<ItemEntity>> itemEntitiesByRequestId = itemRepository
-                .findAllByRequestIdIn(requestIdSet)
-                .stream()
-                .collect(Collectors.groupingBy(ItemEntity::getRequestId));
-
-        return requestEntities.stream()
-                .map(this::entity2dtoResp)
-                .peek(dtoResp -> Optional.ofNullable(dtoResp)
-                        .ifPresent(obj -> obj.setItems(itemMapper.bulkEntity2dtoResp(itemEntitiesByRequestId.get(obj.getId())))))
-                .collect(Collectors.toCollection(LinkedList::new));
-    }
-
-    public List<RequestDtoResp> bulkEntity2dtoResp(List<RequestEntity> requestEntities, Integer requestorId) {
-        Map<Integer, List<ItemForRequestView>> itemsViewByRequestId = itemRepository
-                .findAllByRequesterId(requestorId)
-                .stream()
-                .collect(Collectors.groupingBy(ItemForRequestView::getRequestId));
-
-        return requestEntities.stream()
-                .map(this::entity2dtoResp)
-                .peek(dtoResp -> Optional
-                        .ofNullable(dtoResp)
-                        .ifPresent(obj -> obj.setItems(convert2itemsDtoResp(itemsViewByRequestId.get(obj.getId())))))
-                .collect(Collectors.toCollection(LinkedList::new));
-    }
-
-    private List<ItemDtoResp> convert2itemsDtoResp(List<ItemForRequestView> inList) {
-        if (Objects.isNull(inList))
-            return Collections.emptyList();
-        else
-            return inList.stream().map(ItemForRequestView::getItemDtoResp).collect(Collectors.toList());
     }
 }
--- src/main/java/ru/practicum/shareit/request/service/RequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/service/RequestServiceImpl.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.item.model.ItemDtoResp;
+import ru.practicum.shareit.item.model.ItemEntity;
 import ru.practicum.shareit.item.model.ItemMapper;
+import ru.practicum.shareit.item.repository.ItemForRequestView;
 import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.request.model.RequestDtoReq;
 import ru.practicum.shareit.request.model.RequestDtoResp;
@@ -14,7 +17,8 @@ import ru.practicum.shareit.request.repository.RequestRepository;
 import ru.practicum.shareit.user.repository.UserRepository;
 import ru.practicum.shareit.utils.exception.NotFoundException;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -40,10 +44,19 @@ public class RequestServiceImpl implements RequestService {
     public List<RequestDtoResp> findAllByUser(Integer requestorId) {
         checkUser(requestorId);
 
-        return requestMapper.bulkEntity2dtoResp(
-                requestRepository.findAllByRequestorIdOrderByCreatedDesc(requestorId),
-                requestorId
-        );
+        List<RequestEntity> requestEntities = requestRepository.findAllByRequestorIdOrderByCreatedDesc(requestorId);
+
+        Map<Integer, List<ItemForRequestView>> itemsViewByRequestId = itemRepository
+                .findAllByRequesterId(requestorId)
+                .stream()
+                .collect(Collectors.groupingBy(ItemForRequestView::getRequestId));
+
+        return requestEntities.stream()
+                .map(requestMapper::entity2dtoResp)
+                .peek(dtoResp -> Optional
+                        .ofNullable(dtoResp)
+                        .ifPresent(obj -> obj.setItems(convert2itemsDtoResp(itemsViewByRequestId.get(obj.getId())))))
+                .collect(Collectors.toCollection(LinkedList::new));
     }
 
     @Override
@@ -51,11 +64,20 @@ public class RequestServiceImpl implements RequestService {
     public List<RequestDtoResp> findAllByOthers(Integer requestorId, Integer offset, Integer pageSize) {
         checkUser(requestorId);
 
-        List<RequestEntity> entityList = requestRepository
+        List<RequestEntity> requestEntities = requestRepository
                 .findAllByRequestorIdNotOrderByCreatedDesc(requestorId, PageRequest.of(offset / pageSize, pageSize))
                 .toList();
 
-        return requestMapper.bulkEntity2dtoResp(entityList);
+        Set<Integer> requestIdSet = requestEntities.stream().map(RequestEntity::getId).collect(Collectors.toSet());
+
+        Map<Integer, List<ItemEntity>> itemEntitiesByRequestId = itemRepository
+                .findAllByRequestIdIn(requestIdSet)
+                .stream()
+                .collect(Collectors.groupingBy(ItemEntity::getRequestId));
+
+        return requestEntities.stream().map(requestMapper::entity2dtoResp).peek(dtoResp -> Optional.ofNullable(dtoResp)
+                        .ifPresent(obj -> obj.setItems(itemMapper.bulkEntity2dtoResp(itemEntitiesByRequestId.get(obj.getId())))))
+                .collect(Collectors.toCollection(LinkedList::new));
     }
 
     @Override
@@ -75,5 +97,12 @@ public class RequestServiceImpl implements RequestService {
     private void checkUser(Integer requestorId) {
         if (!userRepository.existsById(requestorId))
             throw new NotFoundException("no such user", String.valueOf(requestorId));
+    }
+
+    private List<ItemDtoResp> convert2itemsDtoResp(List<ItemForRequestView> inList) {
+        if (Objects.isNull(inList))
+            return Collections.emptyList();
+        else
+            return inList.stream().map(ItemForRequestView::getItemDtoResp).collect(Collectors.toList());
     }
 }
