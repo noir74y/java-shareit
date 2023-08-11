2037120a235bc1b3558c2fdd63b4859687d8165a
noir74y <noir74@yandex.ru>
2023-08-11 21:30:14 +0300

    ref: codestyle

--- lib/src/main/java/ru/practicum/shareit/model/comment/CommentDtoResp.java
+++ lib/src/main/java/ru/practicum/shareit/model/comment/CommentDtoResp.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
-import ru.practicum.shareit.utils.AppConfiguration;
 
 import java.time.LocalDateTime;
 
--- lib/src/main/java/ru/practicum/shareit/model/request/RequestDtoResp.java
+++ lib/src/main/java/ru/practicum/shareit/model/request/RequestDtoResp.java
@@ -1,13 +1,10 @@
 package ru.practicum.shareit.model.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import ru.practicum.shareit.model.item.ItemDtoResp;
-import ru.practicum.shareit.utils.AppConfiguration;
 
 import java.time.LocalDateTime;
 import java.util.List;
