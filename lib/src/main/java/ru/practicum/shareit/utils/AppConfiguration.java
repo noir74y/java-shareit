0d09113c67c894441bcf70235ee71c3f4527aa10
noir74y <noir74@yandex.ru>
2023-08-11 21:27:00 +0300

    fix: no matters

--- lib/src/main/java/ru/practicum/shareit/utils/AppConfiguration.java
+++ lib/src/main/java/ru/practicum/shareit/utils/AppConfiguration.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 
 public class AppConfiguration {
     public static final String HEADER_USER_ID = "X-Sharer-User-Id";
-    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'hh:mm:ss";
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
     public static LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER = new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT));
     public static final String OFFSET_DEFAULT = "0";
     public static final String PAGE_SIZE_MAX = "999";
