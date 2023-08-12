828e2b917c74a66bcf89bf2f98f426234572332e
noir74y <noir74@yandex.ru>
2023-08-12 11:54:35 +0300

    ref: to add constant to BaseClient

--- gateway/src/main/java/ru/practicum/shareit/clients/BaseClient.java
+++ gateway/src/main/java/ru/practicum/shareit/clients/BaseClient.java
@@ -4,6 +4,7 @@ import org.springframework.http.*;
 import org.springframework.lang.Nullable;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
+import ru.practicum.shareit.utils.AppConfiguration;
 
 import java.util.List;
 import java.util.Map;
@@ -110,7 +111,7 @@ public class BaseClient {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
         if (userId != null) {
-            headers.set("X-Sharer-User-Id", String.valueOf(userId));
+            headers.set(AppConfiguration.HEADER_USER_ID, String.valueOf(userId));
         }
         return headers;
     }
